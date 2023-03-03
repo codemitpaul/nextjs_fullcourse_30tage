7024025dc6e224c25668f7097f2dff8d35b0fbbd
Paul Mill <codemitpaul@Pauls-MacBook-Pro.local>
2023-03-03 20:06:54 +0100

    tag5

--- models/Post.js
+++ models/Post.js
@@ -1,5 +1,7 @@
+// Importieren der Mongoose-Bibliothek
 import mongoose from "mongoose";
 
+// Definition des Post-Schemas mit zwei Feldern: Name (String, erforderlich) und Beschreibung (String, optional)
 const postSchema = new mongoose.Schema(
   {
     name: {
@@ -11,7 +13,9 @@ const postSchema = new mongoose.Schema(
       required: false,
     },
   },
+  // Optionales Konfigurationsobjekt, das die automatische Generierung von Zeitstempeln ermöglicht
   { timestamps: true }
 );
 
+// Exportieren des Modells mit dem Namen "Post". Wenn das Modell bereits existiert, wird es verwendet, andernfalls wird es erstellt.
 export default mongoose.models.Post || mongoose.model("Post", postSchema);
