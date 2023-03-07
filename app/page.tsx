27d0f70962d5ac5a22befb8b6cfaad08cf504072
Paul Mill <codemitpaul@Pauls-MacBook-Pro.local>
2023-03-07 11:24:39 +0100

    tag16

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import axios from "axios";
 import React from "react";
 
-const Homepage = () => {
-  return <div className="p-5">Homepage</div>;
+const Homepage = async () => {
+  const { data } = await axios.get("https://fakestoreapi.com/products");
+  return (
+    <div className="p-5">
+      {data.map((product: any) => (
+        <div key={product.id}>{product.title}</div>
+      ))}
+    </div>
+  );
 };
 
 export default Homepage;
